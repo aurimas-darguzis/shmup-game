7af6f202b41aae057c4443f564ede958e1e588c0
aurimas-darguzis <aurimas@darguzis.com>
2017-11-29 21:32:16 +0000

    update

---
+++ src/prefabs/NumberBox.js
@@ -0,0 +1,23 @@
+export default class NumberBox extends Phaser.Group {
+  constructor (game, bgasset, val, parent) {
+    super (game, parent)
+    this.create(0, 0, bgasset)
+
+    const style = {
+      font: '30px Arial',
+      align: 'center',
+      fill: '#fff'
+    }
+
+    this.txtValue = new Phaser.Text(
+      this.game, 55, 55, val.toString(), style
+    )
+
+    this.txtValue.anchor.setTo(.5, .5)
+    this.add(this.txtValue)
+  }
+
+  setValue (val) {
+    this.txtValue.text = val.toString()
+  }
+}
